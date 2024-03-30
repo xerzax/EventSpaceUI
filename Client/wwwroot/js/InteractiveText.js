b0cf4f08291b714237fc53478b42116bdf7025a1
Ritika Shrestha <ritustha707@gmail.com>
2024-03-30 13:40:08 +0545

    event view and indexs static

---
+++ Client/wwwroot/js/InteractiveText.js
@@ -0,0 +1,10 @@
+﻿// interactiveText.js
+window.addEventListener('scroll', function () {
+    var element = document.querySelector('.interactive-text');
+    var position = element.getBoundingClientRect();
+    if (position.top >= 0 && position.bottom <= window.innerHeight) {
+        element.classList.add('larger', 'scrolled');
+    } else {
+        element.classList.remove('larger', 'scrolled');
+    }
+});
